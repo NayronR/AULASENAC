e41272c72b498fe7756b295aa0576e0fa9418a36
Nayronr <nayronruansntalto1@gmail.com>
2023-03-30 08:18:41 -0300

    more changes

---
+++ atividadeapp/models.py
@@ -0,0 +1,27 @@
+from distutils.command import upload
+from django.db import models
+
+
+  
+# declare a new model with a name "GeeksModel"
+class Animal(models.Model):
+ 
+    # fields of the model
+    nome = models.CharField(max_length = 200)
+    peso= models.TextField()
+    altura = models.CharField(max_length = 200)
+    descricao = models.CharField(max_length = 200)
+    imagem = models.ImageField(upload_to = 'img', blank = True, null = True)
+ 
+ 
+    # with their title name
+    def __str__(self):
+        return self.nome
+
+class Categoria(models.Model):
+ 
+    # fields of the model
+    nome = models.CharField(max_length = 200) 
+    def __str__(self):
+        return self.nome
+    
